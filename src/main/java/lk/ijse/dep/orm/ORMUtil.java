2f01a66dd392a5f234e65397639452ba83718115
Suchira Liyanage <suchiraliyanage94@gmail.com>
2021-01-20 09:42:56 +0530

    Setting up the connections from the properties

--- src/main/java/lk/ijse/dep/orm/ORMUtil.java
+++ src/main/java/lk/ijse/dep/orm/ORMUtil.java
@@ -1,5 +1,8 @@
 package lk.ijse.dep.orm;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class ORMUtil {
@@ -9,6 +12,19 @@ public class ORMUtil {
         String password = dbProperties.getProperty("javax.persistence.jdbc.password");
         String url = dbProperties.getProperty("javax.persistence.jdbc.url");
         String driverClassName = dbProperties.getProperty("javax.persistence.jdbc.driver_class");
+        Connection connection = null;
 
+        if (username == null || password == null || url == null || driverClassName == null) {
+            throw new RuntimeException("Unable to initialize ORM framework without user-name, password, url, and driver");
+        }
+
+        try {
+            Class.forName(driverClassName);
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException throwables) {
+            throw new RuntimeException("Failed to establish the connection", throwables);
+        }
     }
 }
